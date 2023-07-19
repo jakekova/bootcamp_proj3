cd124a18a0ba814ac2a5ac0ba5da037d9b824e06
nlemunyon <128091997+nlemunyon@users.noreply.github.com>
2023-07-19 08:56:44 -0400

    Add files via upload

--- Web_app/app (1).js
+++ Web_app/app (1).js
@@ -1,37 +1,51 @@
-
-let selYears = d3.select("#selYears")
-
-function init() {
-    let html = "<option>all-time</option>"
-    for (let i = 2023; i >= 1996; i--) {
-        html += `<option>${i}</option>`
-    }
-
-    selYears.html(html)
-    plotTop10('all-time')
-}
-
-function plotTop10(year) {
-    d3.json(`http://localhost:5000/api/top10/${year}`)
-        .then(function(apiData) {
-            console.log(apiData)
-            let trace = {
-                x: apiData.map(company => `<a href="company.html?company_id=${company.company_id}">${company.company}</a>`),
-                y: apiData.map(company => company.revenue),
-                type: 'bar'
-            }
-
-            let layout = {
-                title: `Top 10: ${year}`
-            }
-
-            Plotly.newPlot('plot', [trace], layout)
-        })
-}
-
-selYears.on('change', function() {
-    plotTop10(selYears.property('value'))
-})
-
-
-init()
+function plotAverageAgeByDevice(data) {
+    // Extract unique device values
+    let uniqueDevices = [...new Set(data.map(user => user.Device))];
+    console.log("Test");
+  
+    // Calculate average age for each unique device
+    let averageAges = uniqueDevices.map(device => {
+      let deviceUsers = data.filter(user => user.Device === device);
+      let ages = deviceUsers.map(user => user.Age);
+      let sumOfAges = ages.reduce((total, age) => total + age, 0);
+      let averageAge = sumOfAges / deviceUsers.length;
+      return { Device: device, AverageAge: averageAge };
+    });
+  
+    // Sort the average ages by device name
+    averageAges.sort((a, b) => a.Device.localeCompare(b.Device));
+  
+    // Extract device names and average ages for plotting
+    let deviceNames = averageAges.map(item => item.Device);
+    let averageAgeValues = averageAges.map(item => item.AverageAge);
+  
+    // Plot the average ages by device
+    let trace = {
+      x: deviceNames,
+      y: averageAgeValues,
+      type: 'bar'
+    };
+  
+    let layout = {
+      title: 'Average User Age by Device',
+      xaxis: {
+        title: 'Device'
+      },
+      yaxis: {
+        title: 'Average Age'
+      }
+    };
+  
+    Plotly.newPlot('plot', [trace], layout);
+  }
+  
+  fetch('http://localhost:5000/api/users')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      plotAverageAgeByDevice(data);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+  
