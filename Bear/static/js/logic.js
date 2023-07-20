5ffcab7c2703e1b71215282d667685262e3a771d
Bear St. Michael <128632655+bearstmichael@users.noreply.github.com>
2023-07-20 18:09:16 -0400

    Leaflet up and working, geojson files for all countries, and then just our contries, also found in here.

---
+++ Bear/static/js/logic.js
@@ -0,0 +1,74 @@
+// Creating the map object
+let myMap = L.map("map", {
+    center: [40, -75],
+    zoom: 3
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  // THIS IS WHERE I'M AT --> HOW TO MAKE 'LINK' REFER TO THE GEOJSON FILE I ALREADY HAVE
+  let link = "static/data/countries_narrowed.geojson";
+  
+  // The function that will determine the color of a neighborhood based on the borough that it belongs to
+  function chooseColor(country) {
+    if (country == "Australia") return "red";
+    else if (country == "Brazil") return "orange";
+    else if (country == "Canada") return "yellow";
+    else if (country == "France") return "green";
+    else if (country == "Germany") return "blue";
+    else if (country == "Italy") return "purple";
+    else if (country == "Mexico") return "pink";
+    else if (country == "Spain") return "violet";
+    else if (country == "United Kingdon") return "brown";
+    else if (country == "United States of America") return "blue";
+    else return "black";
+  }
+  
+  // Getting our GeoJSON data
+  d3.json(link).then(function(data) {
+    console.log(data)
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data, {
+      // Styling each feature (in this case, a country)
+      style: function(feature) {
+        return {
+          color: "white",
+          // Call the chooseColor() function to decide which color to color our country. (The color is based on the country.)
+          fillColor: chooseColor(feature.properties.ADMIN),
+          fillOpacity: 0.5,
+          weight: 1.5
+        };
+      },
+      // This is called on each feature.
+      onEachFeature: function(feature, layer) {
+        // Set the mouse events to change the map styling.
+        layer.on({
+          // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 90% so that it stands out.
+          mouseover: function(event) {
+            layer = event.target;
+            layer.setStyle({
+              fillOpacity: 0.9
+            });
+          },
+          // When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), the feature's opacity reverts back to 50%.
+          mouseout: function(event) {
+            layer = event.target;
+            layer.setStyle({
+              fillOpacity: 0.5
+            });
+          },
+          // When a feature (country) is clicked, it enlarges to fit the screen.
+          click: function(event) {
+            myMap.fitBounds(event.target.getBounds());
+          }
+        });
+        // Giving each feature a popup with information that's relevant to it
+        layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.ISO_A3 + "</h2>");
+  
+      }
+    }).addTo(myMap);
+  });
+  
